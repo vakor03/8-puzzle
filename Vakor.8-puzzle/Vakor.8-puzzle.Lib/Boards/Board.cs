f8cf4d9cdccc4234865bef58acbfbc0cb66286ea
Vakor <myksv200@gmail.com>
2021-09-15 10:32:40 +0300

    15.09 Boards

---
+++ Vakor.8-puzzle/Vakor.8-puzzle.Lib/Boards/Board.cs
@@ -0,0 +1,40 @@
+﻿using Vakor._8_puzzle.Lib.Tiles;
+
+namespace Vakor._8_puzzle.Lib.Boards
+{
+    public class Board : IBoard
+    {
+        public ITile[,] GameField => _gameField;
+
+        public ITile this[int x, int y] => _gameField[x, y];
+
+        private readonly ITile[,] _gameField;
+
+
+        public Board()
+        {
+            _gameField = new ITile[IBoard.Dimension, IBoard.Dimension];
+        }
+
+        public void Init()
+        {
+            PutTilesOnRightPlaces();
+        }
+
+
+        public void MixTiles()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private void PutTilesOnRightPlaces()
+        {
+            for (int i = 0; i < IBoard.Dimension * IBoard.Dimension - 1; i++)
+            {
+                _gameField[i / IBoard.Dimension, i % IBoard.Dimension] = new UsualTile(i + 1) {HasRightPlace = true};
+            }
+
+            _gameField[IBoard.Dimension, IBoard.Dimension] = new EmptyTile {HasRightPlace = true};
+        }
+    }
+}
---
+++ Vakor.8-puzzle/Vakor.8-puzzle.Lib/Boards/IBoard.cs
@@ -0,0 +1,14 @@
+﻿using Vakor._8_puzzle.Lib.Tiles;
+
+namespace Vakor._8_puzzle.Lib.Boards
+{
+    public interface IBoard
+    {
+        ITile[,] GameField { get; }
+        ITile this[int x, int y] { get; }
+        const int Dimension = 3;
+        
+        void Init();
+        void MixTiles();
+    }
+}
