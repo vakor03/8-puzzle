b43e0a740528c8cd27484f2f8b2c3007762458ee
Vakor-PC <myksv2003@gmail.com>
2021-09-28 14:05:45 +0300

    28.10 ConsoleApp

---
+++ Vakor.8-puzzle/ConsoleApp1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Vakor._8_puzzle.Lib.Boards;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IBoard board = new Board();
+            
+        }
+    }
+}
