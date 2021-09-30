47b00d298483ef825d2e670957b6353b1ab8e30b
Vakor <myksv200@gmail.com>
2021-09-30 10:20:05 +0300

    30.09 Coordinate

--- Vakor.8-puzzle/Vakor.8-puzzle.Lib/Coordinates/Coordinate.cs
+++ Vakor.8-puzzle/Vakor.8-puzzle.Lib/Coordinates/Coordinate.cs
@@ -1,8 +1,22 @@
-﻿namespace Vakor._8_puzzle.Lib.Coordinates
+﻿using System;
+
+namespace Vakor._8_puzzle.Lib.Coordinates
 {
-    public struct Coordinate
+    public struct Coordinate:IComparable<Coordinate>
     {
         public int X { get; set; }
         public int Y { get; set; }
+
+        public Coordinate(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+        public int CompareTo(Coordinate other)
+        {
+            var xComparison = X.CompareTo(other.X);
+            if (xComparison != 0) return xComparison;
+            return Y.CompareTo(other.Y);
+        }
     }
 }
