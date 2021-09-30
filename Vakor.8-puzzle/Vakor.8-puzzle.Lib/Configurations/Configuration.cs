e69ab212ef629f09001ee0f6c2ee4bfe78868905
Vakor <myksv200@gmail.com>
2021-09-30 10:22:07 +0300

    30.09 Configuration

---
+++ Vakor.8-puzzle/Vakor.8-puzzle.Lib/Configurations/Configuration.cs
@@ -0,0 +1,12 @@
+﻿namespace Vakor._8_puzzle.Lib.Configurations
+{
+    public static class Configuration
+    {
+        public static int Dimension { get; set; }
+
+        static Configuration()
+        {
+            Dimension = 3;
+        }
+    }
+}
