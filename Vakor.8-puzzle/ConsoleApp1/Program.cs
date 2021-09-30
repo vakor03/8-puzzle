5344737628133806eca89662e6a426ce1849fdc4
Vakor <myksv2003@gmail.com>
2021-09-30 22:43:40 +0300

    30.09 Program

--- Vakor.8-puzzle/ConsoleApp1/Program.cs
+++ Vakor.8-puzzle/ConsoleApp1/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
-using Vakor._8_puzzle.Lib.Boards;
+using Vakor._8_puzzle.Lib.Algorithms;
+using Vakor._8_puzzle.Lib.Configurations;
+using Vakor._8_puzzle.Lib.States;
 
 namespace ConsoleApp1
 {
@@ -7,8 +9,34 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            IBoard board = new Board();
+            IState<string> state = State<string>.CreateDefaultState();
+            state.MixTiles();
+            ShowState(state);
+            SolveAlgorithm<string> algorithm = new AStar<string>();
             
+            Console.WriteLine(algorithm.SolvePuzzle(state));
+            Console.WriteLine(algorithm.IterationsCount);
+
+            SolveAlgorithm<string> algorithm1 = new LDFS<string>(25);
+
+            Console.WriteLine(algorithm1.SolvePuzzle(state));
+            Console.WriteLine(algorithm1.IterationsCount);
+
+        }
+
+        private static void ShowState(IState<string> state)
+        {
+            string separator = " ";
+            for (int i = 0; i < Configuration.Dimension; i++)
+            {
+                Console.WriteLine();
+                for (int j = 0; j < Configuration.Dimension; j++)
+                {
+                    Console.Write($"{state[i,j].Data}{separator}");
+                }
+            }
+
+            Console.WriteLine();
         }
     }
 }
